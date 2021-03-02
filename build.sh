3b0d7609a1bcb618800a4050f56c833f90a5528d
Ivan Buetler <ivan.buetler@compass-security.com>
2021-03-02 11:58:13 +0100

    fixing docker

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-rm -rf ./docker-alpine-libwebsockets-with-libuv
-git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
-
-rm -rf ./app
-mv ./docker-alpine-libwebsockets-with-libuv/app .
-
 docker build --no-cache -t hackinglab/alpine-ttyd-bash:3.2.0 -t hackinglab/alpine-ttyd-bash:3.2 -t hackinglab/alpine-ttyd-bash:latest -f Dockerfile .
-
-rm -rf ./docker-alpine-libwebsockets-with-libuv
 
